a510a2014801f468d49b96cb9c34fefcb023ef39
Colin Smith <devcolin@icloud.com>
2019-04-24 21:28:30 -0600

    Second commit

---
+++ VAAST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VAAST
+//
+//  Created by Colin Smith on 4/24/19.
+//  Copyright © 2019 Colin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
